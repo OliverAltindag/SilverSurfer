971e26240eef7b4dd4882d215f5caabad88fc22c
OliverAltindag <85311607+OliverAltindag@users.noreply.github.com>
2026-01-09 18:35:59 -0500

    Update slidingwindow.py to increase size

--- slidingwindow.py
+++ slidingwindow.py
@@ -5,8 +5,8 @@ def create_sliding_windows(
     ricker_vr,       # Shape: (n_scales, full_time_length)
     haar_br,         # Shape: (full_time_length,)
     labels,          # Shape: (full_time_length, 1) or None
-    window_size=128, # Must match CNN input layer, 128
-    stride=32        # Overlap = 96. Ensures every switchback is centered at least once.
+    window_size=256, # UPDATED: Expanded to 256 for Encounter 5 events
+    stride=64        # UPDATED: Increased stride to maintain 75% overlap
 ):
     """
     Slices continuous time-series tensors into overlapping windows for the CNN.
@@ -31,15 +31,14 @@ def create_sliding_windows(
     # we is me
     # dimensions: (Batch_Size, Height/Scales, Width/Time, Channels)
     
-    # Ricker: (Batch, 64, 128, 1)
+    # Ricker: (Batch, 64, Window, 1)
     batch_r_br = np.zeros((num_windows, n_scales, window_size, 1), dtype='float32')
     batch_r_vr = np.zeros((num_windows, n_scales, window_size, 1), dtype='float32')
     
-    # Haar: (Batch, 128, 1) - Note: Haar has no 'Scale' dimension here
-    # is fixed later in pipeline
+    # Haar: (Batch, Window, 1)
     batch_h_br = np.zeros((num_windows, window_size, 1), dtype='float32')
     
-    # Local Labels: (Batch, 128, 1) - The precise mask
+    # Local Labels: (Batch, Window, 1)
     batch_lbls = np.zeros((num_windows, window_size, 1), dtype='float32')
     
     # the sliding Loop
@@ -48,13 +47,11 @@ def create_sliding_windows(
         # Define the window boundaries
         start = i
         end = i + window_size
-        # Input shape: (Scales, Time) -> Slice: (Scales, Window_Size)
-        # assign this into the 4D array at [idx, :, :, 0]
-        # automatically handles the reshaping to add the Channel dimension
+        
         batch_r_br[idx, :, :, 0] = ricker_br[:, start:end]
         batch_r_vr[idx, :, :, 0] = ricker_vr[:, start:end]
         batch_h_br[idx, :, 0] = haar_br[start:end]
-        # labels for if training
+        
         if labels is not None:
             batch_lbls[idx, :, 0] = labels[start:end, 0]
         idx += 1
@@ -64,7 +61,6 @@ def create_sliding_windows(
     # the global label for that window is 1
     # lowkey didnt work terribly
     if labels is not None:
-        # check if ANY value in the window is > 0.5
         Y_global = np.any(batch_lbls > 0.5, axis=(1, 2)).astype('float32').reshape(-1, 1)
     else:
         Y_global = None
